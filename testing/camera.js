5b4b0759c080d21ec79b6c28022a6a42069475ad
Matías <mattyyb96@gmail.com>
2024-12-12 19:57:55 -0300

    camera movements

--- testing/camera.js
+++ testing/camera.js
@@ -6,9 +6,17 @@ export class Camera{
         this.width = width
         this.height = height
         this.x = 0
-        this.y = 32
+        this.y = 0
+        this.maxX = map.image.width - this.width
+        this.maxY = map.image.height - this.height
+        this.speed = 0.2
+    }
+    move(deltaTime, speedX,speedY){
+        this.x += speedX * this.speed * deltaTime
+        this.y += speedY * this.speed * deltaTime
 
-
+        this.x = Math.max(0, Math.min(this.x, this.maxX))
+        this.y = Math.max(0, Math.min(this.y, this.maxY))
     }
 
 
--- testing/game.js
+++ testing/game.js
@@ -4,27 +4,64 @@ import { Camera } from "./camera.js"
 console.log("Welcome to Shadow and Steel")
 
 export const TILE_SIZE = 32
-export const COLS = 15
-export const ROWS = 20
+export const COLS = 15 
+export const ROWS = 20 
 
 export const HALF_SIZE = TILE_SIZE /2
-const GAME_WIDTH = TILE_SIZE * COLS
-const GAME_HEIGHT = TILE_SIZE * ROWS
+
+
+/* const GAME_WIDTH = TILE_SIZE * COLS
+const GAME_HEIGHT = TILE_SIZE * ROWS */
+
+const GAME_WIDTH = 200
+const GAME_HEIGHT = 200
 
      class Game{
 
         constructor(){
             this.map = new Map()
             this.camera = new Camera(this.map, GAME_WIDTH, GAME_HEIGHT) 
-   
+            this.keys= []
+            
+            window.addEventListener("keydown", e =>{
+         
+                if (this.keys.indexOf(e.key)=== -1){
+                    this.keys.unshift(e.key)  
+                }
+                console.log(this.keys)
+    
+            })
+
+             window.addEventListener("keyup", e =>{
+
+                const index = this.keys.indexOf(e.key)
+
+                if (index > -1){
+
+                    this.keys.splice(index, 1)
+                }
+                console.log(this.keys)
+        })
+         
         }
+        update(deltaTime){
+            let speedX = 0;
+            let speedY = 0;
+            if (this.keys[0]=== "ArrowLeft") speedX = -1
+            else if  (this.keys[0]=== "ArrowRight") speedX = 1
+            else if  (this.keys[0]=== "ArrowUp") speedY = -1
+            else if  (this.keys[0]=== "ArrowDown") speedY = 1
+            this.camera.move(deltaTime, speedX,speedY)
+
+        }
+
          render(ctx){
             ctx.drawImage(
                 this.map.image,
-                0,
-                0,
-                500,
-                500,
+                this.camera.x,
+                this.camera.y,
+                GAME_WIDTH,
+                GAME_HEIGHT,
                 0,
                 0,
                 GAME_WIDTH,
@@ -47,7 +84,19 @@ window.addEventListener("load", function(){
 
     const game = new Game ()
 
-    game.render(ctx)
+    let lastTime = 0
 
+    function animate (timeStamp){
+   
 
+        const deltaTime = timeStamp -lastTime
+        lastTime = timeStamp
+
+        console.log(deltaTime)
+
+        game.update(deltaTime)
+        game.render(ctx)
+        requestAnimationFrame(animate)
+    }
+      requestAnimationFrame(animate) 
 })
