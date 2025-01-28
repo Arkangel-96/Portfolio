2b3c4ad28c20b6debbcf91056ff8807614ae7cde
Arkangel-96 <mattyyb96@gmail.com>
2025-01-28 19:34:07 -0300

    idle anim

---
+++ Unity project/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{   
+    private Rigidbody2D rb;
+    private Animator animator;
+    public float moveSpeed = 100;
+    public Vector2 input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent <Rigidbody2D> ();
+        animator = GetComponent <Animator> ();
+    }
+    void FixedUpdate(){
+        if (input.x !=0 ) input.y = 0;
+        rb.velocity = new Vector2(input.x * moveSpeed * Time.deltaTime, input.y * moveSpeed * Time.deltaTime);
+        if (rb.velocity != Vector2.zero){
+
+            animator.SetFloat("moveX", input.x);
+            animator.SetFloat("moveY", input.y);
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+}
