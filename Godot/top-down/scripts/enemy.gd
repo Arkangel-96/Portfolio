942b0b06100e245839b181090438b6a8b18a4b06
maty <mattyyb96@gmail.com>
2025-03-02 02:01:36 -0300

    porting to godot part 2

---
+++ Godot/top-down/scripts/enemy.gd
@@ -0,0 +1,51 @@
+
+class_name Enemy extends CharacterBody2D
+
+var move_speed:= 20
+var attack_damage:= 10
+var is_attack:= false
+
+
+@onready var player: Player = $"../Player"
+
+@onready var sprite_animation : AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	if !is_attack and player:
+		sprite_animation.play("run")
+	
+	var move_direction = (player.position - position).normalized()
+	if move_direction:
+		velocity = move_direction * move_speed
+		if move_direction.x !=0:
+			sprite_animation.flip_h = move_direction.x < 0
+			$AreaAttack.scale.x = 1 if move_direction.x < 0 else -1
+		
+		move_and_slide()
+	
+	
+		
+func attack():
+	sprite_animation.play("attack")
+	is_attack = true 
+	
+
+func _on_area_attack_body_entered(body: Node2D) -> void:
+	if body is Player:
+		attack()
+
+
+func _on_area_attack_body_exited(body: Node2D) -> void:
+	if body is Player:
+		is_attack = false
+		
+		
+		
+
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if sprite_animation.animation == "attack":
+		if is_attack:
+			attack()
+			
---
+++ Godot/top-down/scripts/world.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+const ENEMY = preload("res://scenes/Enemy.tscn")
+@onready var player: Player = $Player
+@onready var timer_spawn_enemy: Timer = $TimerSpawnEnemy
+
+func _ready() -> void:
+	timer_spawn_enemy.wait_time = 3
+	timer_spawn_enemy.start()
+	timer_spawn_enemy.timeout.connect(spawn_enemy)
+
+
+func spawn_enemy():
+	var enemy = ENEMY.instantiate()
+	
+	var random_angle: float = randf() * PI *2
+	var spawn_distance: float = randf_range(270,300)
+	var spawn_offset: Vector2 = Vector2(cos(random_angle),sin(random_angle)) * spawn_distance
+	
+	enemy.position =spawn_offset + player.position
+	
+	add_child(enemy)
