469c84af40c1a3e1f6a14cf81f096bf8a715c3a4
Matías Baltieri <mattyyb96@gmail.com>
2025-03-05 22:54:37 -0300

    Porting to Godot
    porting to Godot part 1 and 2

--- Godot/top-down/scripts/enemy.gd
+++ Godot/top-down/scripts/enemy.gd
@@ -4,13 +4,19 @@ class_name Enemy extends CharacterBody2D
 var move_speed:= 20
 var attack_damage:= 10
 var is_attack:= false
-
+var in_attack_Player_range := false
 
 @onready var player: Player = $"../Player"
-
+@onready var health_component: HealthComponent = $Components/HealthComponent
 @onready var sprite_animation : AnimatedSprite2D = $AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
+func _ready() -> void:
+	health_component.death.connect(on_death)
+	if player:
+		player.attack_finished.connect(verify_receive_damage)
+	
+	
+func _physics_process(delta: float) -> void: 
 	if !is_attack and player:
 		sprite_animation.play("run")
 	
@@ -28,7 +34,14 @@ func _physics_process(delta: float) -> void:
 func attack():
 	sprite_animation.play("attack")
 	is_attack = true 
-	
+
+func verify_receive_damage():
+	if in_attack_Player_range:
+		health_component.receive_damage(player.attack_damage)
+		
+
+func on_death():
+	queue_free()
 
 func _on_area_attack_body_entered(body: Node2D) -> void:
 	if body is Player:
@@ -46,6 +59,7 @@ func _on_area_attack_body_exited(body: Node2D) -> void:
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if sprite_animation.animation == "attack":
+		player.health_component.receive_damage(attack_damage)
 		if is_attack:
 			attack()
 			
--- Godot/top-down/scripts/player.gd
+++ Godot/top-down/scripts/player.gd
@@ -1,17 +1,30 @@
 class_name Player extends CharacterBody2D
 
 @onready var sprite_animation : AnimatedSprite2D = $AnimatedSprite2D
+@onready var health_component: HealthComponent = $Components/HealthComponent
 
+signal attack_finished
 
 var move_speed:= 100
 var attack_damage:= 50
 var is_attack:= false
 
+func _ready() -> void:
+	health_component.death.connect(on_death)
+	
+	
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton :
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
 				attack()
+	if event is InputEventMouseButton :
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			if event.pressed:
+				attack_2()		
+				
+				
+				
 				
 func _physics_process(delta: float) -> void:
 	if is_attack == false:
@@ -38,18 +51,34 @@ func _physics_process(delta: float) -> void:
 func attack():
 	sprite_animation.play("attack")
 	is_attack = true
+	
+func attack_2():
+	sprite_animation.play("attack_2")
+	is_attack = true
 
-
+func on_death():
+	print("GAME OVER")
+	get_tree().paused = true
+	
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if sprite_animation.animation == "attack":
+		attack_damage = 50
 		is_attack = false 
-		
+		attack_finished.emit()
+	if sprite_animation.animation == "attack_2":
+		attack_damage = 100
+		is_attack = false 
+		attack_finished.emit()
 	
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body is Enemy:
+		body.in_attack_Player_range = true
+		
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body is Enemy:
+		body.in_attack_Player_range = false
+		
