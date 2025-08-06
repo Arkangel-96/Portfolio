16138f02c3c55711910cf718508db32d1cbe4238
Matías Baltieri <mattyyb96@gmail.com>
2025-08-06 20:44:49 -0300

    less spawners + yellow enemies

--- Godot/top-down/scenes/enemy/building/spawner.gd
+++ Godot/top-down/scenes/enemy/building/spawner.gd
@@ -3,14 +3,16 @@ extends Node2D
 const BOOMER = preload("res://scenes/enemy/Boomer.tscn")
 const TECHIE = preload("res://scenes/enemy/Techie.tscn")
 const GOBLIN = preload("res://scenes/enemy/Goblin.tscn")
+const GOBLIN_YELLOW = preload("res://scenes/enemy/Goblin_yellow.tscn")
+const TECHIE_YELLOW = preload("res://scenes/enemy/Techie_yellow.tscn")
+const BOOMER_YELLOW = preload("res://scenes/enemy/Boomer_yellow.tscn")
+
 var spawn_points = []
-
-
 @onready var world = get_node("/root/World")
 @onready var marker_2d: Marker2D = $Marker2D
 
-var spawn = [GOBLIN, TECHIE,BOOMER]
-var type = randi_range(0,2)
+var spawn = [GOBLIN, TECHIE, BOOMER, GOBLIN_YELLOW , TECHIE_YELLOW , BOOMER_YELLOW]
+var type = randi_range(0,5)
 var datos
 
 func _on_timer_timeout() -> void:
